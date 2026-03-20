67de05f84efad04d228131932b00ce762bc796f8
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2026-03-19 22:03:02 -0300

    Primeiro commit react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import api from './api';
 
 function App() {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    api.get("/posts")
+      .then( resposta => setPosts(resposta.data))
+  },[])
+
+  const pegaPosts = (e) => {
+    e.preventDefault();
+    api.get("/posts")
+      .then( resposta => setPosts(resposta.data))
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={pegaPosts}>Pegar Posts</button>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post, i) => 
+          <tr key={i}>
+            <td>{post.id}</td>
+            <td>{post.title}</td>
+            <td>{post.author}</td>
+          </tr>
+          )}
+      </tbody>
+      </table>
     </div>
   );
 }
